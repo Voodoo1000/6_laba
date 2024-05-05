30465526190f4dd8a7a0bf25d5c0f1cbdf38f790
Voodoo <vladbutakov08@gmail.com>
2024-05-05 16:10:01 +0800

    Добавление частиц, гравитиции и черной дыры

--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,51 @@
+using System.Runtime.InteropServices;
+
 namespace _6_laba
 {
     public partial class Form1 : Form
     {
+        List<Particle> particles = new();
+        Emitter emitter = new();
+
+        private int MousePositionX = 0;
+        private int MousePositionY = 0;
+
         public Form1()
         {
             InitializeComponent();
+
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+
+            emitter.gravityPoints.Add(new Point(
+                picDisplay.Width / 2, picDisplay.Height / 2
+            ));
+
+            emitter.gravityPoints.Add(new Point(
+              (int)(picDisplay.Width * 0.75), picDisplay.Height / 2
+           ));
+
+            emitter.gravityPoints.Add(new Point(
+               (int)(picDisplay.Width * 0.25), picDisplay.Height / 2
+           ));
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            emitter.UpdateState();
+
+            using (var g = Graphics.FromImage(picDisplay.Image))
+            {
+                g.Clear(Color.Black);
+                emitter.Render(g);
+            }
+
+            picDisplay.Invalidate();
+        }
+        
+        private void picDisplay_MouseMove(object sender, MouseEventArgs e)
+        {
+            emitter.MousePositionX = e.X;
+            emitter.MousePositionY = e.Y;
         }
     }
 }
